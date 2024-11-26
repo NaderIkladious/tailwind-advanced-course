9c075fdc3132742d76543fcb6c44552dd697f0f9
Nader Ikladious <n.abdelmessih@metro-markets.de>
2024-11-26 23:48:27 +0100

    Hide Header and Footer for App

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,7 +2,7 @@ import {Button} from "@/app/components/button";
 
 export const Header = () => {
 	return (
-		<div className="w-full h-16 bg-blue-900 flex items-center justify-between px-4">
+		<div className="w-full h-16 bg-blue-900 flex items-center justify-between px-4 group-data-[traffic=app]/marketplace:hidden">
 			<h1 className="text-2xl">Mini Marketplace</h1>
 			<Button color='white' variant="pill">Login</Button>
 		</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ import {Navigation} from "@/app/components/navigation";
 import {RecentlyBoughtCarousel} from "@/app/components/recently-bought-carousel";
 import {MostBoughtCarousel} from "@/app/components/most-bought-carousel";
 import {Footer} from "@/app/components/footer";
+import {isApp} from "@/app/utils/isApp";
 export default function Home() {
   return (
-    <div>
+    <div className="group/marketplace" data-traffic={isApp()? 'app': 'web'}>
       <Header/>
 
       <div className="mx-4 mt-10 gap-10 flex flex-col">
