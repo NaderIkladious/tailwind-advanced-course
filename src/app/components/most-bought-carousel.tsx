ab4d4c27c3e6674177122c359ca414107114dcb7
Nader Ikladious <n.abdelmessih@metro-markets.de>
2024-11-27 00:15:30 +0100

    Dark themed product tags for app

--- src/app/components/most-bought-carousel.tsx
+++ src/app/components/most-bought-carousel.tsx
@@ -3,7 +3,7 @@ import {Carousel} from "@/app/components/carousel";
 
 export const MostBoughtCarousel = () => {
 	return (
-		<div className="app:[&>h4]:border-b-4 app:[&>h4]:border-blue-800">
+		<div className="app:[&>h4]:border-b-4 app:[&>h4]:border-blue-800 app:[&_.tag]:bg-zinc-800 app:[&_.tag]:text-white">
 			<Carousel title="Most Bought" products={mostBought}></Carousel>
 		</div>
 	)
