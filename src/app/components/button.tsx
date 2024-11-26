d6b9a105010660cd37c697c543834aa4337c04b0
Nader Ikladious <n.abdelmessih@metro-markets.de>
2024-11-26 21:30:37 +0100

    Adjust button code layout for exc-1

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -6,7 +6,9 @@ type Props = {
 }
 export const Button = ({ children }: PropsWithChildren<Props>) => {
 	return (
-		<button className={cls("bg-blue-500 py-1.5 px-4 text-lg hover:bg-blue-700 transition")}>
+		<button
+			className={cls("bg-blue-500 py-1.5 px-4 text-lg hover:bg-blue-700 transition")}
+		>
 			{children}
 		</button>
 	)
