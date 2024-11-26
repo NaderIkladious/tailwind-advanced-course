20cf5be9cbee44019419300d90208af655850c9b
Nader Ikladious <n.abdelmessih@metro-markets.de>
2024-11-26 21:33:45 +0100

    Adjust cls function for exc-1

--- src/app/utils/cls.ts
+++ src/app/utils/cls.ts
@@ -1,7 +1,21 @@
-export const cls = (input) =>
+export const clx = (input) =>
 	input
 		?.replace?.(/\s+/gm, ' ')
 		.split(' ')
 		.filter((cond) => typeof cond === 'string')
 		.join(' ')
 		.trim() ?? ''
+
+export function cls(
+	...args: ([input: string, enabled: boolean] | string)[]
+): string {
+	return args
+		.reduce<string>(
+			(acc, curr) =>
+				typeof curr === 'string'
+					? `${acc}${clx(curr)} `
+					: `${acc}${curr?.[1] ? `${clx(curr?.[0])} ` : ''}`,
+			''
+		)
+		.trim()
+}
