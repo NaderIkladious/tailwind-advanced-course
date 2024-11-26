b0c07353f89a470c39947bdb9cba82158f39f3f0
Nader Ikladious <n.abdelmessih@metro-markets.de>
2024-11-27 00:17:24 +0100

    Clean tag for project

--- src/app/components/product.tsx
+++ src/app/components/product.tsx
@@ -19,7 +19,7 @@ export const Product = ({product}: Props) => {
 				<p className="text-zinc-900 text-lg hover:text-blue-600">{product.title}</p>
 				<ul className="flex gap-2 flex-wrap mt-4">
 					{product.attributes.map(item => (
-						<li className="tag text-zinc-800 border border-zinc-400 rounded px-2" key={item}>{item}</li>
+						<li className="text-zinc-800 border border-zinc-800 rounded px-2" key={item}>{item}</li>
 					))}
 				</ul>
 				<p className="text-zinc-900 text-sm/6 mt-4">{product.description}</p>
